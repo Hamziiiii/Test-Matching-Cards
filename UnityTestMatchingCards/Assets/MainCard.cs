9dc99db3ef018821f3a122ba1bc7dd0e38802c6c
Hamziiiii <56000647+Hamziiiii@users.noreply.github.com>
2020-05-28 21:06:59 +0500

    MatchingCardsLevel1
    -Level 1 with score and game logic implemented.
    -Still needs a popup at the finish of a game.

---
+++ UnityTestMatchingCards/Assets/MainCard.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCard : MonoBehaviour
+{
+
+    [SerializeField] private SceneController controller;
+    public GameObject cardBack;
+
+    public void OnMouseDown()
+    {
+        if (cardBack.activeSelf && controller.canReveal)
+        {
+            cardBack.SetActive(false );
+            controller.CardRevealed(this);
+        }
+    }
+
+    private int _id;
+    public int id
+    {
+        get { return _id; }
+    }
+
+    public void ChangeSprite(int id, Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image; 
+
+    }
+
+    public void Unreveal()
+    {
+        cardBack.SetActive(true);
+    }
+
+}
